11d294d305fa67223b57426cb915c74eed64bff9
J <Dreamscent@home>
2020-06-23 04:28:22 +0800

    Pewpewpew
    More shells achieved

--- Vulnserver/4. KSTET/boofuzz_KSTET.py
+++ Vulnserver/4. KSTET/boofuzz_KSTET.py
@@ -11,19 +11,19 @@ def main():
     session = Session(target=Target(
         connection=SocketConnection(host, port, proto=proto)))
 
-    s_initialize("HTER")  # just giving our session a name, "HTER"
+    s_initialize("KSTET")  # just giving our session a name, "HTER"
 
     # these strings are fuzzable by default, so here instead of blank, we specify 'false'
-    s_string("HTER", fuzzable=False)
+    s_string("KSTET", fuzzable=False)
 
     # we don't want to fuzz the space between "TRUN" and our arg
     s_delim(" ", fuzzable=False)
 
     # This value is arbitrary as we did not specify 'False' for fuzzable. Boofuzz will fuzz this string now
-    s_string("FUZZ")
+    s_string("KSTET")
 
     # having our 'session' variable connect following the guidelines we established in "TRUN"
-    session.connect(s_get("HTER"))
+    session.connect(s_get("KSTET"))
     session.fuzz()  # calling this function actually performs the fuzzing
 
 
